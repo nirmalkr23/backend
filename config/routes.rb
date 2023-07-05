b43ff5700e1f98bfcba06b02f26667fa86b462a9
nirmalkr23 <nirmalworkspace@gmail.com>
2023-07-05 11:08:50 +0530

    crud is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  #root 'home#index'
   get 'home/index'
-
   # namespace :api do
   #   namespace :v1 do
   #     resources :articles, only: [:index,:show,:create,:update,:destroy]
